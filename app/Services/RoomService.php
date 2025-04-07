558514299b03dd4faa5664e670e4823cd7039b23
latifi777 <abdelalilatifi3@gmail.com>
2025-04-07 16:17:32 +0100

    implintation de RoomService

---
+++ app/Services/RoomService.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Services;
+
+use App\Models\Room;
+use Illuminate\Http\Request;
+
+class RoomService
+{
+    public function getAllRooms()
+    {
+        return Room::all();
+    }
+
+    public function storeRoom(Request $request): void
+    {
+        $path = $request->file('images')->store('rooms', 'public');
+
+        Room::create([
+            'room_number' => $request->room_number,
+            'type' => $request->type,
+            'price' => $request->price,
+            'status' => $request->status,
+            'images' => $path,
+        ]);
+    }
+
+    public function deleteRoom(Room $room): void
+    {
+        $room->delete();
+    }
+
+    public function updateRoom(Request $request, Room $room): void
+    {
+        $room->update($request->only('type', 'price', 'status'));
+    }
+}
