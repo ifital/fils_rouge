94fd8f0eaa82a8c3673b1cb3206c95f5f338f5cc
Latifi777 <abdelalilatifi3@gmail.com>
2025-04-09 11:44:50 +0100

    résourdre des soucis pour les fichiers requests

--- app/Http/Requests/StoreRoomRequest.php
+++ app/Http/Requests/StoreRoomRequest.php
@@ -6,19 +6,11 @@ use Illuminate\Foundation\Http\FormRequest;
 
 class StoreRoomRequest extends FormRequest
 {
-    /**
-     * Determine if the user is authorized to make this request.
-     */
     public function authorize(): bool
     {
-        return false;
+        return true; // ou appliquer une logique d'autorisation
     }
 
-    /**
-     * Get the validation rules that apply to the request.
-     *
-     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
-     */
     public function rules(): array
     {
         return [
